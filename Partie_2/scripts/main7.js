cc3e6dabe32c7040f6462d954101a3ccff3d6f16
Véro Brossard <brossard.vero.pro@laposte.net>
2021-01-05 21:32:38 +0100

    amélioration exo 7 affichage pwd

---
+++ Partie_2/scripts/main7.js
@@ -0,0 +1,16 @@
+// var myInput2 = document.getElementById("psw2");
+// var  testPwd = document.getElementById("testPwd");
+
+// console.log(myInput.value, myInput2.value,testPwd.classList)
+
+// When the user clicks outside the password field n°2, start the comparison.
+// myInput2.onblur = function() {
+//   if(myInput.value == myInput2.value) {
+//     testPwd.classList.remove("invalid");
+//     testPwd.classList.add("valid");
+//   } else {
+//     testPwd.classList.remove("valid");
+//     testPwd.classList.add("invalid");
+//   }
+//   console.log(myInput.value, myInput2.value,testPwd.classList)
+// }
